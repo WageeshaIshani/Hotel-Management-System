68d8c4a047895d062939a8d86bb487bba972c8ca
WageeshaIshani <wagee.ishani@gmail.com>
2017-04-04 20:09:41 +0530

    Object Oriented Version SDP-CW2

---
+++ Customer.java
@@ -0,0 +1,18 @@
+
+public class Customer {
+
+	private String customer_name;
+	
+	
+	public String getCustomer_name() {
+		return customer_name;
+	}
+
+	
+	public void setCustomer_name(String customer_name) {
+		this.customer_name = customer_name;
+	}
+
+
+	
+}
---
+++ Room.java
@@ -0,0 +1,20 @@
+
+public class Room {
+	
+	private int room_no;
+	
+	
+	public int getRoom_no() {
+		return room_no;
+	}
+
+
+
+	public void setRoom_no(int room_no) {
+		this.room_no = room_no;
+	}
+
+
+
+
+}
